fdd742d1b94c242b4b9c01d73bfb51d8f43e7234
S.Shramko <shramko.web@yahoo.com>
2021-02-03 21:52:16 +0200

    feat: add articles & my templates

--- src/express/routes/root.js
+++ src/express/routes/root.js
@@ -4,13 +4,13 @@ const {Router} = require(`express`);
 
 const rootRouter = new Router();
 
-rootRouter.get(`/`, (req, res) => res.render(`main`));
-rootRouter.get(`/categories`, (req, res) => res.render(`all-categories`, {
+rootRouter.get(`/`, (req, res) => res.render(`root/main`));
+rootRouter.get(`/categories`, (req, res) => res.render(`root/all-categories`, {
   classNames: [`wrapper`, `wrapper--nobackground`]
 }));
-rootRouter.get(`/login`, (req, res) => res.render(`login`));
-rootRouter.get(`/register`, (req, res) => res.render(`sign-up`));
-rootRouter.get(`/search`, (req, res) => res.render(`search`,
+rootRouter.get(`/login`, (req, res) => res.render(`root/login`));
+rootRouter.get(`/register`, (req, res) => res.render(`root/sign-up`));
+rootRouter.get(`/search`, (req, res) => res.render(`root/search`,
     {classNames: `wrapper-color`}
 ));
 
