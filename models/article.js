69a93ec52d1a2060c817b837145a0d7b12ff531c
s.shramko <shramko.web@yahoo.com>
2021-04-11 13:48:00 +0300

    feat: define article model

---
+++ models/article.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const {Model, DataTypes} = require(`sequelize`);
+
+const define = (sequelize) => {
+  class Article extends Model {
+  }
+
+  Article.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [30, 250],
+      },
+    },
+    announce: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [30, 250],
+      },
+      allowNull: false,
+    },
+    fullText: {
+      type: DataTypes.STRING(1000),
+    },
+    photo: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    sequelize,
+    modelName: `Article`,
+    tableName: `articles`,
+  });
+
+};
+
+module.exports = define;
