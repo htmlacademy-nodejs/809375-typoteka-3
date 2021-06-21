3d11f118406a0a96f10cbfe93af7a1fc9eda77a6
Serhii Shramko <shramko.web@yahoo.com>
2021-06-21 22:16:22 +0300

    fix: change logger destination in test mode

--- src/service/logger.js
+++ src/service/logger.js
@@ -6,6 +6,7 @@ const path = require(`path`);
 const {Env, ProjectPath} = require(`../constants`);
 
 const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
+const isTestMode = process.env.NODE_ENV === Env.TEST;
 const defaultLogLevel = isDevMode ? `info` : `error`;
 const LOG_FILE = path.resolve(ProjectPath.ROOT_FOLDER, `logs/api.log`);
 
@@ -13,7 +14,7 @@ const logger = pino({
   name: `base-logger`,
   level: process.env.LOG_LEVEL || defaultLogLevel,
   prettyPrint: isDevMode,
-}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
+}, isDevMode || isTestMode ? process.stdout : pino.destination(LOG_FILE));
 
 module.exports = {
   logger,
